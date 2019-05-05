9984f35c5ef47b53b9a83778203ff89023647d3b
yoshihiro-oguni <38971031+yoshihiro-oguni@users.noreply.github.com>
2019-05-05 14:45:24 +0900

    py_jsのアンケートの修正
    htmlの特殊文字の変換のミスや通信エラーの表示エラーなど

--- markdown/py_js_src/src/questionnaire.ts
+++ markdown/py_js_src/src/questionnaire.ts
@@ -1,13 +1,21 @@
-const url = "https://script.google.com/macros/s/AKfycbyZ8GSp5pl1Gb82gxPu_fndx4hSFo6mEUUsiJBSamiQhRmtz6k/exec"
-
 const escapeHTMLSpecialChars = (str: string): string => {
-    return str.replace(/</g, "&lt;")
+    return str.replace(/&/g, "&amp;")
+              .replace(/</g, "&lt;")
               .replace(/>/g, "&gt;")
-              .replace(/&/g, "&amp;")
-              .replace(/"/, "&quot;")
-              .replace(/'/, "&#x27;")
-              .replace(/\//, "&#x2F;");
+              .replace(/"/g, "&quot;")
+              .replace(/'/g, "&#x27;")
+              .replace(/\//g, "&#x2F;");
 };
+
+const questionnaireAlertProgressTemplate = (id: string, selection: string, details: string) => {
+    return `
+<h1>以下のデータを送信しています...</h1>
+節: ${id}<br>
+選択: ${selection}<br>
+詳細: ${details === "" ? "なし （詳細を書いてくれると助かります！）" : escapeHTMLSpecialChars(details)}`;
+};
+
+const url = "https://script.google.com/macros/s/AKfycbyZ8GSp5pl1Gb82gxPu_fndx4hSFo6mEUUsiJBSamiQhRmtz6k/exec";
 
 // 送信に成功したらPromiseの結果がtrue
 // 失敗したらfalseまたはエラー（catchが必要）
@@ -18,11 +26,11 @@ const post = (data: { [key: string]: unknown }): Promise<boolean> => {
 
     data.id = id;
     const options: RequestInit = {
-        method: "POST",
-        mode: "no-cors",
+        body: JSON.stringify(data),
         cache: "no-cache",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data),
+        method: "POST",
+        mode: "no-cors",
     };
     return fetch(url, options)
         .then(() => fetch(url))
@@ -33,63 +41,65 @@ const post = (data: { [key: string]: unknown }): Promise<boolean> => {
         });
 };
 
-const disableAllButtons = () => {
+const showAlert = () => {
     Array.prototype.forEach.call(document.querySelectorAll(".questionnaire button"), (button: HTMLButtonElement) => {
         button.disabled = true;
     });
 };
 
-const enableAllButtons = () => {
-    Array.prototype.forEach.call(document.querySelectorAll(".questionnaire button"), (button: HTMLButtonElement) => {
-        button.disabled = false;
-    });
+const hideAlert = (box: HTMLElement, messageHTML: string) => {
+    box.innerHTML = messageHTML;
+    setTimeout(() => {
+        box.classList.remove("connection-status-box--show");
+        Array.prototype.forEach.call(
+            document.querySelectorAll(".questionnaire button"),
+            (button: HTMLButtonElement) => {
+                button.disabled = false;
+            });
+    }, 800);
 };
 
 Array.prototype.forEach.call(document.getElementsByClassName("questionnaire"), (questionnaire: unknown) => {
-        if (!(questionnaire instanceof HTMLElement)) {
-            return;
-        }
-        if ((window as any).platform.name !== "Chrome" &&
-                (window as any).platform.name !== "Firefox" &&
-                (window as any).platform.name !== "Microsoft Edge") {
-            // Safariは動作確認できないのでIEと同じ処理
-            questionnaire.innerHTML = "";
-            return;
-        }
-        Array.prototype.forEach.call(questionnaire.getElementsByTagName("button"), (button: unknown) => {
-            if (!(button instanceof HTMLButtonElement)) {
-                return;
-            }
-            button.addEventListener("click", () => {
-                disableAllButtons();
+    if (!(questionnaire instanceof HTMLElement)) {
+        return;
+    }
 
-                const details = escapeHTMLSpecialChars(questionnaire.querySelector("textarea")!.value);
-                const box = document.getElementsByClassName("connection-status-box")[0];
-                if (!(box instanceof HTMLElement)) {
-                    throw new Error("'.connection-status-box' was not found");
+    // IEとかならアンケートを表示しない
+    if ((window as any).platform.name !== "Chrome" &&
+            (window as any).platform.name !== "Firefox" &&
+            (window as any).platform.name !== "Microsoft Edge") {
+        // Safariは動作確認できないのでIEと同じ処理
+        questionnaire.innerHTML = "";
+        return;
+    }
+
+    Array.prototype.forEach.call(questionnaire.getElementsByTagName("button"), (button: unknown) => {
+        if (!(button instanceof HTMLButtonElement)) {
+            return;
+        }
+        button.addEventListener("click", () => {
+            showAlert();
+            const details = questionnaire.querySelector("textarea")!.value;
+            const box = document.getElementsByClassName("connection-status-box")[0];
+            if (!(box instanceof HTMLElement)) {
+                throw new Error("'.connection-status-box' was not found");
+            }
+            box.classList.add("connection-status-box--show");
+            box.innerHTML = questionnaireAlertProgressTemplate(questionnaire.id, button.innerText, details);
+            post({
+                details,
+                sectionId: questionnaire.id,
+                text: button.innerText,
+            }).then((ok: boolean) => {
+                if (!ok) {
+                    hideAlert(box, `<h1>送信に失敗しました。</h1>`);
+                    return;
                 }
-                box.classList.add("connection-status-box--show");
-                box.innerHTML = `
-<h1>以下のデータを送信しています...</h1>
-節: ${questionnaire.id}<br>
-選択: ${button.innerText}<br>
-詳細: ${details === "" ? "なし （詳細を書いてくれると助かります！）" : details}`;
-                post({
-                    details,
-                    sectionId: questionnaire.id,
-                    text: button.innerText,
-                }).then((ok: boolean) => {
-                    box.innerHTML = `<h1>送信に成功しました。</h1>`;
-                    setTimeout(() => {
-                        box.classList.remove("connection-status-box--show");
-                        enableAllButtons();
-                    }, 800);
-                }).catch((err) => {
-                    box.innerHTML = `<h1>エラーが発生しました</h1>${err}`;
-                    box.classList.remove("connection-status-box--show");
-                    console.error(err);
-                    enableAllButtons();
-                });
+                hideAlert(box, `<h1>送信に成功しました。</h1>`);
+            }).catch((err: unknown) => {
+                hideAlert(box, `<h1>エラーが発生しました。</h1>${escapeHTMLSpecialChars("" + err)}`);
+                console.error(err);
             });
         });
     });
+});
